d0cdedbf57113cb49bd49bcd7b5d48c7883b1128
Tobby324 <B00165442@mytudublin.ie>
2026-04-23 22:44:57 +0100

    Blog 1 Week 10 - Set up front-end dependencies with Laravel Mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,8 @@
               integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
               crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <!-- Bootstrap 5 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-        <!-- jQuery and Bootstrap 3 JS for dropdown -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="{{asset('js/app.js')}}"></script>
     </head>
 
     <body>
@@ -55,9 +50,6 @@
             </div> 
         </div> 
 
-        <!-- Webpack mix npm generated -->
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
-        <script src="{{asset('js/app.js')}}"></script>
         @stack('js_scripts')
     </body>
 </html>
