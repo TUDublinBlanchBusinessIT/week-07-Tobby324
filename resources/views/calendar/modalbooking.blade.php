a73dbeeba8b8581261f9a796b8712abd98a2ead7
Tobby324 <B00165442@mytudublin.ie>
2026-04-24 10:44:12 +0100

    Blog 5 Week 10 - Updated modal title and finalised booking form

--- resources/views/calendar/modalbooking.blade.php
+++ resources/views/calendar/modalbooking.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="modalTitle">Modal Header</h4>
+                <h4 class="modal-title" id="modalTitle">New Booking</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
