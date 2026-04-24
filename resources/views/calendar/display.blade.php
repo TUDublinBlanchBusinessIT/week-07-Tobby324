111202f24cf1172e44f7ce9390dea7346ba4fecf
Tobby324 <B00165442@mytudublin.ie>
2026-04-24 10:42:05 +0100

    Blog 4 Week 10 - Added modal form to create bookings from calendar

--- resources/views/calendar/display.blade.php
+++ resources/views/calendar/display.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <div id="calendar"></div>
+@include('calendar.modalbooking')
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
@@ -14,9 +15,26 @@
             slotDuration: '00:10:00',
             defaultDate: '2026-04-01',
             editable: true,
-            events: '{{ route('calendar.json') }}'
+            events: '{{ route('calendar.json') }}',
+            dateClick: function(info) {
+                document.getElementById('starttime').value = info.date.toISOString().substring(11, 16);
+                document.getElementById('bookingDate').value = info.date.toISOString().substring(0, 10);
+                var modalElement = document.getElementById('fullCalModal');
+                var modal = new bootstrap.Modal(modalElement);
+                modal.show();
+            },
         });
         calendar.render();
+    });
+
+    document.addEventListener("DOMContentLoaded", () => {
+        document.getElementById("submitButton").addEventListener("click", function (e) {
+            e.preventDefault();
+            const form = document.getElementById("bookingForm");
+            form.submit();
+            const modal = bootstrap.Modal.getInstance(document.getElementById("fullCalModal"));
+            modal.hide();
+        });
     });
 </script>
 @endsection
